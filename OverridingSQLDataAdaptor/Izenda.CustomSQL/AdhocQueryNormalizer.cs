25bea5efb85f454b6f8f89d297103ba8f3c58981
Corey R <corey.robinson@izenda.com>
2017-11-09 13:23:26 -0500

    Initial commit

---
+++ OverridingSQLDataAdaptor/Izenda.CustomSQL/AdhocQueryNormalizer.cs
@@ -0,0 +1,29 @@
+﻿using Izenda.BI.QueryNormalizer.SQL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Izenda.BI.Framework.Models.Contexts;
+using Izenda.BI.Logging.Log4Net;
+using Izenda.BI.Logging;
+
+namespace Izenda.CustomSQL
+{
+    /// <summary>
+    /// This example will allow you to alter the query to add something like nocount or execute as
+    /// This will effect all queries (both to the Izenda config db and your reporting db)
+    /// If you do not add the reporting users to the Izenda database you will need to use something like the table name prefixed by Izenda to not wrap these
+    /// </summary>
+    public class AdhocQueryNormalizer : SQLQueryNormalizerActivity
+    {
+        private readonly ILog Logger = (new LogManager()).GetLogger(typeof(AdhocQueryNormalizer));
+        public override int Order => 100;
+
+        public override void Execute(QueryNormalizerContext context)
+        {
+            Logger.Info($"Overriding the query: {context.Query}");
+
+            context.Query = $"SET NOCOUNT ON; {context.Query} SET NOCOUNT OFF;";
+        }
+    }
+}
---
+++ OverridingSQLDataAdaptor/Izenda.CustomSQL/AdhocSQLConnection.cs
@@ -0,0 +1,26 @@
+﻿using Izenda.BI.Logging;
+using Izenda.BI.Logging.Log4Net;
+using Izenda.BI.SQL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Izenda.CustomSQL
+{
+    /// <summary>
+    /// This is the actual override of the connection string below
+    /// </summary>
+    public class AdhocSQLConnection : SQLConnection
+    {
+        private readonly ILog Logger = (new LogManager()).GetLogger(typeof(AdhocSQLConnection));
+
+        public override IDbConnection OpenConnection(string connectionString)
+        {
+            Logger.Info($"Overriding the connection string {connectionString}");
+
+            return base.OpenConnection(connectionString);
+        }
+    }
+}
